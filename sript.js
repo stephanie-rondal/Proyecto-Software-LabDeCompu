5105ca1033bf797d96df1e4f5d15951aecf0af47
stephanie <2523412@ucc.edu.ar>
2025-06-07 01:26:47 -0300

    calcuadora terminada

--- sript.js
+++ sript.js
@@ -14,15 +14,15 @@ function validar(){
 
 }
 
-// FUNCION DE SELECCIÓN DE OPERACIÓN SEGÚN LOS BOTONES
-let operacionSeleccionada = "";
+// FUNCION DE SELECCIÓN DE OPERACIÓN SEGÚN CLICK EN LOS BOTONES
+let operacionSeleccionada = ""; // Crea una variable vacía temporalmente por que aún no se ingresó algo
 function seleccionarOperacion(op) {
-    operacionSeleccionada = op;
+    operacionSeleccionada = op; // Asigna a la variable la operación del botón seleccionado
 }
 
 // FUNCIÓN DE OPERACIONES ARITMÉTICAS SEGÚN SELECCIÓN
 function operar() {
-    let numero1 = parseInt(document.getElementById("numero1").value);
+    let numero1 = parseInt(document.getElementById("numero1").value); // Toma el valor ingresado en el input mediante su id y lo asigna a una variable de tipo entero
     let numero2 = parseInt(document.getElementById("numero2").value);
     let resultado;
 
@@ -33,10 +33,30 @@ function operar() {
     } else if (operacionSeleccionada == "producto") {
         resultado = numero1 * numero2;
     } else if (operacionSeleccionada == "cociente") {
-        resultado =  numero1 / numero;
+        resultado =  numero1 / numero2;
     } else {
         resultado = "Operación no seleccionada";
     }
 
-    document.getElementById("resultado").value = resultado;
+    document.getElementById("resultado").value = resultado; // Toma el input mediante su id y le asigna un valor 
+}
+
+// FUNCIÓN DE SELECCIÓN DE INPUT
+let campoActivo = "";  // Crea una variable vacía temporalmente. Para saber en qué input se está escrbiendo
+function seleccionarCampo(id) { 
+    campoActivo = id; // Le asigna a la variable el valor del de los input mediante sus id 
+}
+
+// FUNCIÓN DE MOSTRAR NUMEROS SELECCIONADOS EN CADA INPUT
+function agregarNumero(numero) {
+    if (campoActivo) { 
+    document.getElementById(campoActivo).value += numero; // Toma el valor del boton presionado y lo muestra en el input  
+    }
+}
+
+// FUNCIÓN DE VACIAR LOS INPUTS
+function limpiar() {
+    document.getElementById("numero1").value = ""; // Toma al input con id numero1 y le da un valor vacío
+    document.getElementById("numero2").value = "";
+    document.getElementById("resultado").value = "";
 }
